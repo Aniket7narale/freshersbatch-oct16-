b0c44afa581a5c43ccfcb2df5ac46bb09c131cc2
Aniket7narale <97221771+Aniket7narale@users.noreply.github.com>
2022-02-04 12:38:07 +0530

    Add files via upload

---
+++ Core Java/Basic_DS/ArmstrongBetweenRange.java
@@ -0,0 +1,21 @@
+public class ArmstrongBetweenRange {
+    public static void main (String [] args) {
+                  
+
+        for (int k = 100 ; k <= 1000 ; k++){
+            int n = k;
+            int d = 0;
+            int s = 0;
+
+            while (n > 0){
+            d = n % 10;
+            s = s + (d * d * d);
+            n = n / 10;
+            }
+            if (k == s){
+                System.out.println (k + "Is Armstrong Number");
+            }
+        }
+    }
+    
+}
